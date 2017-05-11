markitondemand/moment-msdate

commit 4b4f712615e5bebdd5582c8262c07afa79ed7188

functioning tests

moment-msdate.js
@@ -5,11 +5,11 @@
 	/**
 	* Constants
 	*/
-	var DAY_MILLISECONDS = 86400000,
-		MINUTE_MILLISECONDS = 60000,
-		MS_DAY_OFFSET = 25569;
+	const DAY_MILLISECONDS = 86400000;
+	const MINUTE_MILLISECONDS = 60000;
+	const MS_DAY_OFFSET = 25569;
 
-	var moment = (typeof require !== 'undefined' && require !== null) && !require.amd ? require('moment-timezone') : this.moment;
+	const moment = (typeof require !== 'undefined' && require !== null) && !require.amd ? require('moment-timezone') : this.moment;
 
 	/**
 	 * @description To JavaScript date from OLE Automation date
@@ -19,9 +19,9 @@
 	 * @returns moment
 	 */
 	moment.fromOADate = function(msDate, offsetToUtcInMinutes) {
-		let jO = new Date( ((msDate - MS_DAY_OFFSET) * DAY_MILLISECONDS) );
-		let tz = isNaN(parseInt(offsetToUtcInMinutes)) ? jO.getTimezoneOffset() : offsetToUtcInMinutes;
-		jO = new Date( ( (msDate-MS_DAY_OFFSET + (tz / (60*24) ) ) * DAY_MILLISECONDS) );
+		let jO = new Date(((msDate - MS_DAY_OFFSET) * DAY_MILLISECONDS));
+		const tz = isNaN(parseInt(offsetToUtcInMinutes, 10)) ? jO.getTimezoneOffset() : offsetToUtcInMinutes;
+		jO = new Date(((msDate - MS_DAY_OFFSET + (tz / (60 * 24))) * DAY_MILLISECONDS));
 		return moment(jO);
 	};
 
@@ -31,15 +31,15 @@
 	 * @param jsDate A JavaScript date object to convert to OA Date. Defaults to existing moment instance or new Date
 	 * @returns Floating-point number, e.g., 41502.558798240745
 	 */
-	moment.fn.toOADate = function(jsDateInput){
-		let jsDate = jsDateInput || this._d || new Date();
-		let timezoneOffset = jsDate.getTimezoneOffset() / (60 * 24);
-		let msDateObj = ( jsDate.getTime() / DAY_MILLISECONDS ) + (MS_DAY_OFFSET - timezoneOffset);
+	moment.fn.toOADate = function(jsDateInput) {
+		const jsDate = jsDateInput || this._d || new Date();
+		const timezoneOffset = jsDate.getTimezoneOffset() / (60 * 24);
+		const msDateObj = (jsDate.getTime() / DAY_MILLISECONDS) + (MS_DAY_OFFSET - timezoneOffset);
 		return msDateObj;
-	}
+	};
 
 	moment.updateLocale('en', {
-		'invalidDate': undefined
+		invalidDate: undefined
 	});
 
 	/**
@@ -47,9 +47,9 @@
 	* Returns a UTC Moment object instance
 	*/
 	moment.fromOADateWithZone = function(msDate, timeZone) {
-		let jsTicks = (msDate - MS_DAY_OFFSET) * DAY_MILLISECONDS,
-			offset = moment.tz(timeZone).utcOffset() * MINUTE_MILLISECONDS;
-		if(timeZone) {
+		const jsTicks = (msDate - MS_DAY_OFFSET) * DAY_MILLISECONDS;
+		const offset = moment.tz(timeZone).utcOffset() * MINUTE_MILLISECONDS;
+		if (timeZone) {
 			return moment.tz(jsTicks - offset, timeZone).utc();
 		}
 		return moment.utc(jsTicks);
@@ -60,11 +60,11 @@
 	* Returns an OLE Automation date in the form of a double
 	*/
 	moment.fn.toOADateWithZone = function() {
-		let nMsDate = ( this.valueOf() / DAY_MILLISECONDS ) + MS_DAY_OFFSET;
+		const nMsDate = (this.valueOf() / DAY_MILLISECONDS) + MS_DAY_OFFSET;
 		return nMsDate;
 	};
 
-	if ((typeof module !== 'undefined' && module !== null ? module.exports : void 0) != null) {
+	if ((typeof module !== 'undefined' && module !== null ? module.exports : undefined) !== null) {
 		module.exports = moment;
 	}
 

.eslintignore
@@ -1,0 +1,2 @@
+.settings/**
+node_modules

.eslintrc
@@ -1,11 +1,13 @@
 {
-	"env": {
-		"mocha": true,
-		"node": true
-	},
+  "extends": [
+	"@markit/eslint-config-markit",
+	"@markit/eslint-config-markit/node",
+	"@markit/eslint-config-markit/es6"
+	],
 	"rules": {
-		"camelcase": [1, { "properties": "never" }],
-		"eol-last": 0,
-		"quotes": [2, "single"]
+		"brace-style": [2, "1tbs", { "allowSingleLine": true }],
+		"object-shorthand": 0,
+		"spaced-comment": 0,
+		"no-invalid-this": 0
 	}
 }

npm-debug.log
@@ -1,40 +1,0 @@
-0 info it worked if it ends with ok
-1 verbose cli [ 'C:\\Program Files\\nodejs\\node.exe',
-1 verbose cli   'C:\\Program Files\\nodejs\\node_modules\\npm\\bin\\npm-cli.js',
-1 verbose cli   'run',
-1 verbose cli   'test' ]
-2 info using npm@2.14.3
-3 info using node@v4.1.0
-4 verbose run-script [ 'pretest', 'test', 'posttest' ]
-5 info pretest moment-msdate@0.2.0
-6 info test moment-msdate@0.2.0
-7 verbose unsafe-perm in lifecycle true
-8 info moment-msdate@0.2.0 Failed to exec test script
-9 verbose stack Error: moment-msdate@0.2.0 test: `npm run lint && npm run mocha`
-9 verbose stack Exit status 1
-9 verbose stack     at EventEmitter.<anonymous> (C:\Program Files\nodejs\node_modules\npm\lib\utils\lifecycle.js:214:16)
-9 verbose stack     at emitTwo (events.js:87:13)
-9 verbose stack     at EventEmitter.emit (events.js:172:7)
-9 verbose stack     at ChildProcess.<anonymous> (C:\Program Files\nodejs\node_modules\npm\lib\utils\spawn.js:24:14)
-9 verbose stack     at emitTwo (events.js:87:13)
-9 verbose stack     at ChildProcess.emit (events.js:172:7)
-9 verbose stack     at maybeClose (internal/child_process.js:817:16)
-9 verbose stack     at Process.ChildProcess._handle.onexit (internal/child_process.js:211:5)
-10 verbose pkgid moment-msdate@0.2.0
-11 verbose cwd C:\Projects\playground\moment-msdate-open-source
-12 error Windows_NT 6.1.7601
-13 error argv "C:\\Program Files\\nodejs\\node.exe" "C:\\Program Files\\nodejs\\node_modules\\npm\\bin\\npm-cli.js" "run" "test"
-14 error node v4.1.0
-15 error npm  v2.14.3
-16 error code ELIFECYCLE
-17 error moment-msdate@0.2.0 test: `npm run lint && npm run mocha`
-17 error Exit status 1
-18 error Failed at the moment-msdate@0.2.0 test script 'npm run lint && npm run mocha'.
-18 error This is most likely a problem with the moment-msdate package,
-18 error not with npm itself.
-18 error Tell the author that this fails on your system:
-18 error     npm run lint && npm run mocha
-18 error You can get their info via:
-18 error     npm owner ls moment-msdate
-18 error There is likely additional logging output above.
-19 verbose exit [ 1, true ]

test/.eslintrc
@@ -1,0 +1,9 @@
+{
+  "env": {
+    "mocha": true
+  },
+  "rules":{
+    // assert.throws doesnt respond to lambda syntax
+    "prefer-arrow-callback": 0
+  }
+}

test/test.js
@@ -1,34 +1,31 @@
 'use strict';
 
-var assert = require('assert'),
-	lint = require('mocha-eslint'),
-	moment = require('../moment-msdate');
-
-lint(['./*.js', './test/*.js']);
+const assert = require('assert');
+const moment = require('../moment-msdate');
 
 describe('moment-msdate', function() {
 	it('should parse an OLE Automation date int', function(done) {
-		var date = moment.fromOADate(41493);
+		const date = moment.fromOADate(41493);
 		assert.equal(date.toString(), 'Wed Aug 07 2013 00:00:00 GMT-0600');
 		done();
 	});
 
 	it('should parse an OLE Automation date double', function(done) {
-		var date = moment.fromOADate(41493.706892280097000);
+		const date = moment.fromOADate(41493.706892280097000);
 		assert.equal(date.toString(), 'Wed Aug 07 2013 16:57:55 GMT-0600');
 		done();
 	});
 
 	it('should convert an empty JavaScript date to an OLE Automation date of 0', function(done) {
-		var date = new Date(1899, 11, 30, 0, 0, 0);
-		var oaDate = moment(date).toOADate();
+		const date = new Date(1899, 11, 30, 0, 0, 0);
+		const oaDate = moment(date).toOADate();
 		assert.equal(oaDate, 0);
 		done();
 	});
 
 	it('should convert a JavaScript date to an OLE Automation date int', function(done) {
-		var date = new Date(2012, 9, 15);
-		var oaDate = moment(date).toOADate();
+		const date = new Date(2012, 9, 15);
+		const oaDate = moment(date).toOADate();
 		assert.equal(oaDate, 41197);
 		done();
 	});
@@ -43,8 +40,8 @@ describe('moment-msdate', function() {
 });
 
 describe('moment-msdate: moment.fromOADate', function() {
-	it('should convert an OLE Automation date to a moment with a specified offset to UTC', function(){
-		var date = moment.fromOADate(42754.835023148145, 0);
-		console.log('date', date.format())
-	})
+	it('should convert an OLE Automation date to a moment with a specified offset to UTC', function() {
+		const date = moment.fromOADate(42754.835023148145, 0);
+		console.log('date', date.format());
+	});
 });

commit 5ad9d3a7a8e4d684c824b92b3320a8dd6eee4325

Remove strict null check for module.exports
When running moment-msdate in the browser (and without require.js), the library works fine, but throws an exception on this block:

	if ((typeof module !== 'undefined' && module !== null ? module.exports : undefined) != null) {
		module.exports = moment;
	}

The exception reads:

    Uncaught ReferenceError: module is not defined
        at moment-msdate@0.2.0:68
        at moment-msdate@0.2.0:71

You can try this live in a simple HTML page here: <https://cdn.rawgit.com/Zlatkovsky/2f59c552505873f8fd73dc3657b1b9ed/raw/762f060aa5bedc9c044de9995fbfa13a39baa811/index.html>

The issue is that the if statement creates an `undefined` which it then compares with `!== null` and -- since the two don't match -- tries to set an `exports` property on an undefined `module` object.  Fix is to make the equality check be non-strict and skip the `module.exports = moment` line for *both* null and undefined, since it would fail in either case.

It appears that the issue is a regression introduced in January in commit https://github.com/markitondemand/moment-msdate/commit/4b4f712615e5bebdd5582c8262c07afa79ed7188.

Thanks!

moment-msdate.js
@@ -64,7 +64,7 @@
 		return nMsDate;
 	};
 
-	if ((typeof module !== 'undefined' && module !== null ? module.exports : undefined) !== null) {
+	if ((typeof module !== 'undefined' && module !== null ? module.exports : undefined) != null) {
 		module.exports = moment;
 	}
 